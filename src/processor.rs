be31dc85062185bc9f1140566fe360656c1edd28
Diehard073055 <eshanshafeeq073055@gmail.com>
2022-01-10 02:13:51 +0800

    feat cross_program_invocation: Add cross program invocation
    - Invoke the token program to transfer token ownership.

--- src/processor.rs
+++ src/processor.rs
@@ -1,15 +1,15 @@
 use solana_program::{
     account_info::{next_account_info, AccountInfo},
     entrypoint::ProgramResult,
-    program_error::ProgramError,
     msg,
-    pubkey::Pubkey,
+    program::invoke,
+    program_error::ProgramError,
     program_pack::{Pack, IsInitialized},
-    sysvar::{rent::Rent, Sysvar},
+    pubkey::Pubkey,
+    sysvar::{rent::Rent, Sysvar}
 };
 
-use crate::error::EscrowError;
-use crate::instruction::EscrowInstruction;
+use crate::{instruction::EscrowInstruction, error::EscrowError, state::Escrow};
 
 pub struct Processor;
 impl Processor {
@@ -33,15 +33,15 @@ impl Processor {
         program_id: &Pubkey,
     ) -> ProgramResult {
         let account_info_iter = &mut accounts.iter();
-        let initialzer = next_account_info(account_info_iter)?;
+        let initializer = next_account_info(account_info_iter)?;
 
-        if !initialzer.is_signer {
+        if !initializer.is_signer {
             return Err(ProgramError::MissingRequiredSignature);
         }
 
         let temp_token_account = next_account_info(account_info_iter)?;
-        let token_to_recieve_account = next_account_info(account_info_iter)?;
-        if *token_to_recieve_account.owner != spl_token::id(){
+        let token_to_receive_account = next_account_info(account_info_iter)?;
+        if *token_to_receive_account.owner != spl_token::id(){
             return Err(ProgramError::IncorrectProgramId);
         }
 
@@ -57,6 +57,34 @@ impl Processor {
         if escrow_info.is_initialized() {
             return Err(ProgramError::AccountAlreadyInitialized);
         }
+
+        escrow_info.is_initialized = true;
+        escrow_info.initializer_pubkey = *initializer.key;
+        escrow_info.temp_token_account_pubkey = *temp_token_account.key;
+        escrow_info.initializer_token_to_receive_account_pubkey = *token_to_receive_account.key;
+        escrow_info.expected_amount = amount;
+
+        Escrow::pack(escrow_info, &mut escrow_account.try_borrow_mut_data()?)?;
+        let (pda, _bump_seed) = Pubkey::find_program_address(&[b"escrow"], program_id);
+        let token_program = next_account_info(account_info_iter)?;
+        let owner_change_ix = spl_token::instruction::set_authority(
+            token_program.key,
+            temp_token_account.key,
+            Some(&pda),
+            spl_token::instruction::AuthorityType::AccountOwner,
+            initializer.key,
+            &[&initializer.key],
+        )?;
+
+        msg!("Calling the token program to transfer token account ownership...");
+        invoke(
+            &owner_change_ix,
+            &[
+                temp_token_account.clone(),
+                initializer.clone(),
+                token_program.clone(),
+            ],
+        )?;
 
         Ok(())
     }
